c8b3c90acb05bb20144845d5fa6d23f0837dba05
Faaiz Mahmood <faizzafar44@gmail.com>
2025-02-28 20:19:28 +0500

    update CORS

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(bodyParser.json());
 app.use(cors({
-  origin: ['http://localhost:5173', ''],
+  origin: ['http://localhost:5173', 'https://aduu-solar.vercel.app'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true
 }));
